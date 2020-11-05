45d58457b8f5fedea29a2abc826f5c62d742a96e
Anurup Dey <anu.rup.dey98@gmail.com>
2020-11-05 14:51:31 +0530

    Adds server querying, player listing, and auto shutdown

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import os
 
 import boto3
+import asyncio
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from mcstatus import MinecraftServer
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -29,11 +31,68 @@ def get_instance_state():
     return instance
 
 
+class Timer:
+    def __init__(self, timeout, callback, job_args):
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job(job_args))
+
+    async def _job(self, job_args):
+        await asyncio.sleep(self._timeout)
+        await self._callback(*job_args)
+
+    def cancel(self):
+        self._task.cancel()
+
+
+MCSERVERTIMEOUT = 2.5 * 60  # 2.5 mins
+
+
+async def server_stop_timer(ctx, i):
+    # check server status
+    instance = get_instance_state()
+    if instance.state["Name"] == "running":
+        server = MinecraftServer(instance.public_ip_address, 25565)
+        status = server.status()
+        if status.players.online == 0:
+            if i == 0:
+                # check to see if it remains inactive after TIMEOUT
+                Timer(MCSERVERTIMEOUT, server_stop_timer, (ctx, 1))
+            elif i == 1:
+                # it has been 2 * TIMEOUT seconds with no activity
+                # so stop the server
+                await ctx.send(
+                    f"No activity in server for {2*MCSERVERTIMEOUT/60} mins. Stopping Server"
+                )
+                instance.stop()
+                instance.wait_until_stopped()
+                await ctx.send("Server Stopped")
+        else:
+            # players are playing... check again after timout
+            Timer(
+                MCSERVERTIMEOUT,
+                server_stop_timer,
+                (ctx, 0),
+            )
+    else:
+        # nothing to do, the server was shut down by users
+        return
+
+
 @bot.command(name="status", help="Gets the current status of the server")
 async def status(ctx):
     await ctx.send("Checking Server Status")
     instance = get_instance_state()
-    await ctx.send(f"Server Status: {instance.state['Name']}")
+    response = f"Server Status: {instance.state['Name']}"
+    if instance.state["Name"] == "running":
+        response = response + (f"\nIP: {instance.public_ip_address}")
+        server = MinecraftServer(instance.public_ip_address, 25565)
+        status = server.status()
+        response = (
+            response
+            + f"\n{status.players.online} player{' is' if status.players.online == 1 else 's are'} online"
+        )
+    await ctx.send(response)
 
 
 @bot.command(name="start", help="Starts the server")
@@ -43,17 +102,48 @@ async def start(ctx):
     if instance.state["Name"] != "running":
         instance.start()
         instance.wait_until_running()
+
+        Timer(
+            MCSERVERTIMEOUT,
+            server_stop_timer,
+            (ctx, 0),
+        )
     await ctx.send(f"Server running at {instance.public_ip_address}")
 
 
-@bot.command(name="stop", help="Stops the server")
+@bot.command(name="stop", help="Stops the server, only if no players are online")
 async def stop(ctx):
     await ctx.send("Stopping server")
     instance = get_instance_state()
     if instance.state["Name"] == "running":
-        instance.stop()
-        instance.wait_until_stopped()
+        server = MinecraftServer(instance.public_ip_address, 25565)
+        status = server.status()
+        if status.players.online != 0:
+            await ctx.send(
+                f"There is/are {status.players.online} players(s) still playing\n. Can not stop server."
+            )
+            return
+        else:
+            instance.stop()
+            instance.wait_until_stopped()
     await ctx.send("Server is stopped")
+
+
+@bot.command(name="playing", help="Displays the names of players currently playing")
+async def players(ctx):
+    await ctx.send("Checking who is playing")
+    instance = get_instance_state()
+    if instance.state["Name"] == "running":
+        server = MinecraftServer(instance.public_ip_address, 25565)
+        query = server.query()
+        if query.players.online > 0:
+            await ctx.send(
+                "Currently playing:\n {0}".format("\n".join(query.players.names))
+            )
+        else:
+            await ctx.send("No one is playing")
+    else:
+        await ctx.send(f"The server is not running")
 
 
 @bot.command(name="ip", help="Responds with the IP of the server")
